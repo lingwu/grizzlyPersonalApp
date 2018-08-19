aeb775100c6f1e43ad2315592a17ee89e04116c9
wanglili <1057252264@qq.com>
2018-08-19 16:41:56 +0800

    我新建了一個aboutUs文件夾

---
+++ src/views/aboutUs/app.vue
@@ -0,0 +1,61 @@
+<template>
+  <UIFrame
+    title="this is a title just for demo"
+  >
+<a class="mint-cell">
+   <div class="mint-cell-left"></div>
+   <div class="mint-cell-wrapper">
+    <div class="mint-cell-title">
+       <span class="mint-cell-text">标题文字</span>
+    </div>
+  <div class="mint-cell-value"><span></span></div>
+  </div>
+  <div class="mint-cell-right"></div>
+</a>
+  </UIFrame>
+</template>
+
+<script>
+import { LoadMore } from 'muse-ui';
+import { Cell } from 'mint-ui';
+import { DefaultFrame } from 'components';
+export default {
+  data () {
+    return {
+      num: 10,
+      refreshing: false,
+      loading: false,
+      text: 'List'
+    };
+  },
+  components: {
+    UIFrame: DefaultFrame,
+    LoadMore,
+    Cell
+  },
+  methods: {
+    refresh () {
+      this.refreshing = true;
+      this.$refs.container.scrollTop = 0;
+      setTimeout(() => {
+        this.refreshing = false;
+        this.text = this.text === 'List' ? 'Menu' : 'List';
+        this.num = 10;
+      }, 2000);
+    },
+    load () {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+        this.num += 10;
+      }, 2000);
+    }
+  },
+  mounted () {
+  }
+};
+</script>
+<style lang="less" scoped>
+@import url('../../assets/css/base.less');
+
+</style>
